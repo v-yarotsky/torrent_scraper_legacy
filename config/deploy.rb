d70ee3aa14755015aa3679659e48a7a67b94cd05
v.yarotsky <v.yarotsky@itransition.com>
2011-07-03 19:01:25 +0300

    bundle

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,8 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require "rvm/capistrano"                             
+set :rvm_ruby_string, 'ruby-1.9.2-p180'              
+set :rvm_type, :user
+
 set :application, "scraper"
 role :web, "192.168.1.4"
 
@@ -15,7 +20,7 @@ default_run_options[:pty] = true
 set :restart_method, :nginx
 set :rails_env, "production"
 set :rake, "$GEM_HOME/bin/rake"
-set :bundle, "$GEM_HOME/bin/bundle"
+set :bundler, "$GEM_HOME/bin/bundle"
 
 after :deploy, "bundle:install"
 after :deploy, "migrate"
@@ -32,11 +37,11 @@ task :reseed do
 end
 
 task :migrate do
-  run "cd #{deploy_to}/current && #{rake} db:migrate"
+  run "cd #{deploy_to}/current && RAILS_ENV=#{rails_env} #{rake} db:migrate"
 end
 
 namespace :bundle do
   task :install, :roles => :web, :except => { :no_release => true } do
-    run "cd #{deploy_to}/current && #{bundle} install"
+    run "cd #{deploy_to}/current && #{bundler} install --local --without development test"
   end
 end
